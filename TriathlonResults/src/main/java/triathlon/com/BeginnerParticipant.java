cb4ec68d1b9ccc422fee2a9ddacab6a78154ce23
A-M-Ahmed <abdirahmannadaara42@gmail.com>
2025-01-07 03:48:12 +0300

    This is Question Two

---
+++ TriathlonResults/src/main/java/triathlon/com/BeginnerParticipant.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package triathlon.com;
+
+public class BeginnerParticipant extends participant {
+    public BeginnerParticipant(String name, String id, int swimmingTime, int cyclingTime, int runningTime) {
+        super(name, id, swimmingTime, cyclingTime, runningTime);
+    }
+}
+
